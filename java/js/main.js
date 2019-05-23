8d0e1bba6ef755de778bb6eeab773de9b1f60c0c
QedimBabayev <49645572+QedimBabayev@users.noreply.github.com>
2019-05-23 11:36:38 +0400

    Add files via upload

---
+++ java/js/main.js
@@ -0,0 +1,106 @@
+
+// let max = 0;
+
+
+// function Array(numbers)
+// {
+//     for(let i = 0; i<numbers.length; i++)
+//     {
+//       if(numbers[i]>max){
+//           max=numbers[i]
+//       }
+
+
+//     }
+//     // return greatest
+//   return max;
+
+
+// }
+
+//  let greatest = Array([5, 8, 15, 12, 24, 22]);
+
+
+//  console.log (max + "is greatest" )
+// let numbers = [-1,4,3,6,8,2, -100];
+
+// function Array(numbers){
+//     let min = numbers[0];
+//      for( let i= 1; i < numbers.length; i++){
+//         if(numbers[i] < min){
+//             min = numbers[i]
+//         }
+//      }
+//       return min;
+// }
+// let min = Array(numbers);
+
+
+//  console.log( min );
+
+
+// function getminimum(numbers){
+//     let min= numbers[0];
+//     for( let i = 1; i< numbers.length; i++){
+//         if(numbers[i] < min){
+//             min = numbers[i]
+//         }
+//     }
+//     return min;
+
+// }
+// function getmaximum(numbers){
+//     let max= numbers[0];
+//     for( let i = 1; i< numbers.length; i++){
+//         if(numbers[i] > max){
+//             max = numbers[i]
+//         }
+//     }
+//     return max;
+// }
+// function findSum(numbers){
+//     let min = getminimum(numbers);
+//     let max = getmaximum(numbers);
+
+//         return min+max;
+// }
+// let result = findSum ([10, 5, 7, 8, 27]);
+// console.log(result);
+
+
+
+
+
+// function findCount(word, letter) {
+
+
+//    let  count = 0;
+//     for(let i = 0; i < word.length; i++){
+//         if(word[i] === letter)
+//         {
+//             count ++;
+//         }
+//     }
+//     return count;
+// }
+//  let count = findCount ("Azerbycan".toLowerCase(), "A".toLowerCase());
+//  console.log (count);
+
+
+
+// let array = [];
+// let count = prompt();
+// for (let i = 0; i < count; i++) {
+//     let word = prompt("enter name");
+//     array.push(word);
+
+// }
+// console.log(array);
+
+
+
+
+
+
+
+
